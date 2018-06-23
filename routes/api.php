169db261c59a18ae5946c610c2782b9c7bc9114f
shailesh <shailesh.kumarcs@gmail.com>
2018-06-23 22:42:26 +0530

    Producttransaction, Product category and product buyer implemented

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,10 @@ Route::resource('categories.transactions','Category\CategoryTransactionControlle
 * Products
 */
 Route::resource('products','Product\ProductController',['only' => ['index','show']]);
+Route::resource('products.categories','Product\ProductCategoryController',['only' => ['index', 'update','destroy']]);
+Route::resource('products.buyers','Product\ProductBuyerController',['only' => ['index']]);
+Route::resource('products.transactions','Product\ProductTransactionController',['only' => ['index']]);
+
 
 /**
 * Sellers
